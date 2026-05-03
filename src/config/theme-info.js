6ec3b1b1b827642e1df7ce1fefe8294a2703e951
maeeive <anonimobeastw@gmail.com>
2026-05-02 22:39:03 -0300

    Update theme-info.js

---
+++ src/config/theme-info.js
@@ -0,0 +1,4 @@
+export const EXTENSION_NAME = 'Coquette Echoes Theme';
+export const EXTENSION_ID = 'coquette-theme';
+export const EXTENSION_FOLDER_PATH = `scripts/extensions/third-party/${EXTENSION_ID}`;
+export const THEME_VERSION = '3.1.0';
